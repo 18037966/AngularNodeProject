105de3a1ba5a1d636c936d5da8e3e5314930f74d
labib <zbaiyer.labib@gmail.com>
2020-04-28 15:58:39 +1000

    Section-6

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,30 +1,65 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
 
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg'
+};
 //router.post()
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if(isValid){
+      error = null;
+    }
+    cb(null, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
 
 const Post = require('../models/post');
 
-router.post("", (req, res, next) => {
+router.post("", multer({storage: storage}).single("image"),(req, res, next) => {
+    const url = req.protocol + '://' + req.get("host");
     const posts = new Post({
       title: req.body.title,
-      content: req.body.content
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename
     });
     posts.save().then(result => {
       res.status(201).json({
         message: "Post added successfully",
-        postId: result._id
+        post: {
+          id: result._id,
+          title: result.title,
+          content: result.content,
+          imagePath: result.imagePath
+        }
       })
     });
 
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+  console.log(req.file);
+  let imagePath;
+  if(req.file){
+    const url = req.protocol + '://' + req.get("host");
+    imagePath = url + "/images/" + req.file.filename;
+  }
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   })
   Post.updateOne({_id: req.params.id}, post).then(result => {
     console.log(result);
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 //import { Post } from '../post.model';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { PostsService } from "../posts.service";
 import { Post } from "../post.model";
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create', //allows us to which that component
@@ -19,10 +20,17 @@ export class PostCreateComponent implements OnInit{
   private postId: string;
   post: Post;
   isLoading = false;
+  form: FormGroup;
+  imagePreview: string;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute){}
 
   ngOnInit(){
+    this.form = new FormGroup({
+      'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      'content': new FormControl(null, {validators: [Validators.required]}),
+      'image': new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if(paramMap.has('postId')){
           this.mode = 'edit';
@@ -30,7 +38,12 @@ export class PostCreateComponent implements OnInit{
           this.isLoading = true;
           this.postsService.getPost(this.postId).subscribe(postData => {
             this.isLoading = false;
-            this.post = {id: postData._id, title: postData.title, content: postData.content};
+            this.post = {id: postData._id, title: postData.title, content: postData.content, imagePath: postData.imagePath};
+            this.form.setValue({
+              'title': this.post.title,
+              'content': this.post.content,
+              'image': this.post.imagePath
+            });
           });
       }else {
         this.mode = 'create';
@@ -39,19 +52,31 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+    console.log(file);
+    console.log(this.form);
+  }
 
-  onAddPost(form: NgForm){
-    if(form.invalid){
+  onAddPost(){
+    if(this.form.invalid){
       return;
     }
     this.isLoading = true;
-    
+
     if(this.mode === 'create'){
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     }else {
-      this.postsService.updatePost(this.postId,form.value.title, form.value.content )
+      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content, this.form.value.image)
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
 
